66c1a25b8e0a8759e32cb44108ed97eaa8b9ae15
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-08 03:29:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,6 +46,8 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   
+  directors_totals(directors_database)
+  list_of_directors(directors_database)
   
 end
 
